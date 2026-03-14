6a6ed2410f9037ba71ee089fed651081001835aa
Buybanklogs <130286159+Buybanklogs@users.noreply.github.com>
2026-03-13 23:17:42 -0700

    Update Dashboard.js

--- frontend/src/pages/user/Dashboard.js
+++ frontend/src/pages/user/Dashboard.js
@@ -110,14 +110,7 @@ const Dashboard = () => {
             <h1 className="text-4xl font-bold text-slate-900 tracking-tight">Dashboard</h1>
             <p className="text-lg text-slate-600 mt-2">Welcome back to MIC Trades</p>
           </div>
-    /*
-          <Link to="/trade" className="mt-4 lg:mt-0">
-            <Button className="bg-blue-600 hover:bg-blue-700 w-full lg:w-auto shadow-lg" data-testid="trade-now-btn">
-              <TrendingUp className="w-5 h-5 mr-2" />
-              Trade Now
-            </Button>
-          </Link>
-          */
+
         </div>
         
         {loading ? (
